2652b728fb9a64c235ac0c0e272708ae4c981af4
MielAndMoon <dmestasa@gmail.com>
2024-08-31 01:01:45 -0500

    feat: implement a chatbot with gemini

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key=st.secrets["API_KEY"])
 
 st.set_page_config(
     page_title="Turismo Verde | Chatbot",
@@ -13,3 +16,37 @@ if "text_received" not in st.session_state:
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+if "gemini_model" not in st.session_state:
+    st.session_state["gemini_model"] = "gemini-1.5-flash"
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["parts"])
+
+model = genai.GenerativeModel(st.session_state["gemini_model"])
+
+if prompt := st.chat_input("Hazme una pregunta"):
+    st.session_state.messages.append({"role": "user", "parts": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        chat = model.start_chat(
+            history=[
+                {"role": m["role"], "parts": m["parts"]}
+                for m in st.session_state.messages
+            ]
+        )
+
+        stream = chat.send_message(
+            prompt,
+            generation_config=genai.types.GenerationConfig(
+                max_output_tokens=100
+            ),
+        )
+        text = stream.to_dict()["candidates"][0]["content"]["parts"][0]["text"]
+        st.write(text)
+
+    st.session_state.messages.append(
+        {"role": "model", "parts": text})
