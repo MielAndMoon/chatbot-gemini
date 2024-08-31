366226460eaf704c1c9d8213e31ada31308777fb
MielAndMoon <dmestasa@gmail.com>
2024-08-31 00:59:02 -0500

    feat: define layout and states

--- app/main.py
+++ app/main.py
@@ -4,3 +4,12 @@ st.set_page_config(
     page_title="Turismo Verde | Chatbot",
     page_icon="four_leaf_clover"
 )
+
+c1, c2, c3 = st.columns([5, 2, 2], vertical_alignment='bottom')
+c1.title("Turismo Verde")
+
+if "text_received" not in st.session_state:
+    st.session_state["text_received"] = []
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
